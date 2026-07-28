f7f3adabc41d7ccecc5eed3d051d802605153027
JayJayArr <JayArr@outlook.de>
2026-07-28 11:22:51 +0200

    read from key file

---
+++ Makefile
@@ -0,0 +1,7 @@
+.PHONY: build
+build: ## build application
+	cargo build
+
+.PHONY: run
+run: ## run application
+	cargo run
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+
+async fn main() {
+    let file =
+        tokio::fs::read_to_string("key-94e25400-f2ce-42a0-a9b5-44973aa372b9-rietdorf_test.json")
+            .await
+            .unwrap();
+    print!("{}", file);
 }
