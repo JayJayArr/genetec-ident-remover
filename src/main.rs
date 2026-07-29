985339f23d63e3575b7a2e89a90a742d5e09a262
JayJayArr <JayArr@outlook.de>
2026-07-29 14:54:06 +0200

    get and filter identities

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use std::process::exit;
+
 use crate::key::KeyFile;
 use crate::telemetry::init_tracing;
 use clap::Parser;
 use oauth2::basic::{BasicClient, BasicTokenType};
 use oauth2::{ClientId, ClientSecret, EmptyExtraTokenFields, StandardTokenResponse, TokenUrl};
 use oauth2::{TokenResponse, reqwest};
+use reqwest::Client;
+use serde_json::Value;
 use tracing::info;
 use tracing::warn;
 mod key;
@@ -36,7 +40,7 @@ async fn main() -> anyhow::Result<()> {
 
     let key_values: KeyFile = serde_json::from_str(file.as_str())?;
 
-    let tokenresponse = get_api_token(
+    let tokenresponse = get_bearer_token(
         key_values.clientId,
         key_values.clientSecret,
         format!("{}/connect/token", key_values.stsUrl),
@@ -44,16 +48,54 @@ async fn main() -> anyhow::Result<()> {
     .await?;
 
     info!("Token obtained: {}", tokenresponse.access_token().secret());
+
+    let identities_response = get_all_identities(
+        tokenresponse.access_token().secret(),
+        key_values.identityServiceUrl,
+        key_values.accountId,
+    )
+    .await?;
+
+    let mut relevant_identities: Vec<String> = vec![];
+
+    // Iterate over all Identities and extract IDs
+    for identity in identities_response {
+        let id = identity.get("identityId").unwrap().to_string();
+        let email = identity.get("email").unwrap_or_default().to_string();
+        let status = identity.get("status").unwrap_or_default().to_string();
+        let lastmodified = identity
+            .get("lastModificationDateUtc")
+            .unwrap_or_default()
+            .to_string();
+        info!(
+            "Identity found, Status: {}, Id: {}, email: {}, lastmodified: {}",
+            status, id, email, lastmodified
+        );
+
+        if status.contains("Inactive") {
+            relevant_identities.push(id);
+        }
+    }
+
+    info!(
+        "Found a total of {} inactive identities.",
+        relevant_identities.len()
+    );
+
+    if args.dry_run {
+        info!("Dry Run, Aborting. To delete identities rerun without --dry-run");
+        exit(0);
+    }
     Ok(())
 }
 
-async fn get_api_token(
+async fn get_bearer_token(
     client_id: String,
     client_secret: String,
     endpoint: String,
 ) -> anyhow::Result<StandardTokenResponse<EmptyExtraTokenFields, BasicTokenType>> {
     info!("Authenticating with the Genetec API: {}", endpoint);
-    let client = BasicClient::new(ClientId::new(client_id))
+    let oauth_client = BasicClient::new(ClientId::new(client_id))
         .set_client_secret(ClientSecret::new(client_secret))
         .set_token_uri(TokenUrl::new(endpoint)?);
 
@@ -63,11 +105,42 @@ async fn get_api_token(
         .build()
         .expect("Client should build");
 
-    let token_result: StandardTokenResponse<EmptyExtraTokenFields, BasicTokenType> = client
+    let token_result: StandardTokenResponse<EmptyExtraTokenFields, BasicTokenType> = oauth_client
         .exchange_client_credentials()
         .request_async(&http_client)
         .await?;
 
     info!("Authentication successful");
     Ok(token_result)
+}
+
+async fn get_all_identities(
+    bearer_token: &str,
+    identity_base_url: String,
+    account_id: String,
+) -> anyhow::Result<Vec<Value>> {
+    let url = format!(
+        "{}/api/v4/accounts/{}/identities",
+        identity_base_url, account_id
+    );
+
+    info!(
+        "Getting identities for AccountID {} from {}",
+        account_id, url
+    );
+
+    let identity_client = Client::new();
+    let response = identity_client
+        .get(url)
+        .bearer_auth(bearer_token)
+        .send()
+        .await?;
+    let body = response.text().await?;
+    let json: serde_json::Value = serde_json::from_str(&body)?;
+    Ok(json
+        .get("identities")
+        .expect("Could not find field \"identities\" in the json response")
+        .as_array()
+        .expect("Could not convert the Identities in an array")
+        .clone())
 }
